a5e14c50a6ee49c5acaebfe51ef4ed5cf08c6f94
Ricardo H. Ramírez-Gonzalez <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-03-10 11:49:43 +0000

    Added the parse and parseLine methods to compy with the biojs-io-parser specification. Changed all the underscore variables to cammelScript

--- test/biojsiosam_test.js
+++ test/biojsiosam_test.js
@@ -13,22 +13,22 @@ chai.should();
 var biojsiosam = require('../lib/biojsiosam.js');
 
 describe('biojs-io-sam module', function(){
-  describe('#parse_sam_line',function(){
+  describe('#parseSamLine',function(){
   	it('should parse the sam line', function(){
-  		sam_line = biojsiosam.parse_sam_line("test_1	0	chr_1	225	1	70M	*	0	0	TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAAC	IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII	AS:i:0	XS:i:0	XN:i:0	XM:i:0	XO:i:0	XG:i:0	NM:i:0	YT:Z:UU");
-  		sam_line.qname.should.equal("test_1");
-  		sam_line.flags.should.equal(0);
-  		sam_line.rname.should.equal("chr_1");
-  		sam_line.pos.should.equal(225);
-  		sam_line.mapq.should.equal(1);
-  		sam_line.cigar.should.equal("70M");
-  		sam_line.rnext.should.equal("*");
-  		sam_line.pnext.should.equal(0);
-  		sam_line.tlen.should.equal(0);
-  		sam_line.seq.should.equal("TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAAC");
-  		sam_line.qual.should.equal("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
-  		sam_line.full_id().should.equal("test_1")
-  		//console.log(sam_line.full_id());
+  		samLine = biojsiosam.parseLine("test_1	0	chr_1	225	1	70M	*	0	0	TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAAC	IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII	AS:i:0	XS:i:0	XN:i:0	XM:i:0	XO:i:0	XG:i:0	NM:i:0	YT:Z:UU");
+  		samLine.qname.should.equal("test_1");
+  		samLine.flags.should.equal(0);
+  		samLine.rname.should.equal("chr_1");
+  		samLine.pos.should.equal(225);
+  		samLine.mapq.should.equal(1);
+  		samLine.cigar.should.equal("70M");
+  		samLine.rnext.should.equal("*");
+  		samLine.pnext.should.equal(0);
+  		samLine.tlen.should.equal(0);
+  		samLine.seq.should.equal("TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAAC");
+  		samLine.qual.should.equal("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+  		samLine.fullId().should.equal("test_1")
+  		//console.log(samLine.full_id());
   	});
   });
 });
